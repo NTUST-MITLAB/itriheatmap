8ede87fe36d63446fbd14c808dd43d169886d7e8
Syahidah Izza Rufaida <syahidah.izza@gmail.com>
2018-11-29 01:04:01 +0800

    final version for classifier and regression prediction

--- src/classifier-heatmap-generator.py
+++ src/classifier-heatmap-generator.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 from matplotlib import pyplot as plt
@@ -23,7 +23,7 @@ warnings.filterwarnings('ignore')
 
 # # Predicted Data Preparation 
 
-# In[10]:
+# In[2]:
 
 
 x_cut = 50  
@@ -45,58 +45,82 @@ background = get_map_image(black_white=True)
 x_coord_view = [lon for lon in x_coord_list]
 y_coord_view = [lat for lat in y_coord_list]
 
+generated_function = [generate_predicted_data_pci, 
+                      generate_predicted_data_rsrp, 
+                      generate_predicted_data_rsrq, 
+                      generate_predicted_data_snr]
+
 all_x_pci = pd.DataFrame({'location_x':x_coord_list, 'location_y':y_coord_list})
 
-
-# # Predict
-
-# In[12]:
+normalize_rsrq = matplotlib.colors.Normalize(vmin=-9, vmax=-5)
+normalize_snr = matplotlib.colors.Normalize(vmin=0, vmax=30)
 
 
-s = 2
-config = {6 : [s]}
-ml_name = 'xgboost'
+# # Predict 
+
+# In[3]:
+
+
+prediction_columns = ["PCI", "RSRP", "RSRQ", "SNR"]
+# min 1, max 3
+pred_index = 3
+set_val = 26
+base_model = 'xgboost'
+ml_name = 'lgbm'
 training_method = 'baseline'  
-# training_method = 'independent_set_%d' % (s) 
-training_method = 'transfer_except_%d' % (s) 
+training_method = 'independent_set_%d' % (set_val) 
+training_method = 'transfer_except_%d' % (set_val) 
 
 
-# In[26]:
+# In[4]:
 
 
-model_name = 'db/%s_%s_%s' % ('PCI', ml_name, training_method)
+config = {6 : [set_val]}
+pred_col = prediction_columns[pred_index]
+model_name = 'db/%s_%s_%s' % (pred_col, ml_name, training_method)
 model = pickle.load(open(model_name + ".pickle.dat", "rb"))
-all_x_pci_dict = generate_predicted_data_pci(config, all_x_pci, refresh=False)
-
-beam_columns = [c for c in all_x_pci_dict[(6, s)] if "beam" in c]
-all_x_pci_dict = {k:all_x_pci_dict[k].drop(beam_columns + ['set'], axis=1) for k in all_x_pci_dict}
+normalized = [None, None, normalize_rsrq, normalize_snr]
 
 
-# In[29]:
+# In[5]:
 
 
-proba = False
+for s in config[6] :
+    all_x_data = add_features(pd.DataFrame(all_x_pci), 6, s)
+    beam_columns = [c for c in all_x_data if "beam" in c]
+    all_x_data = all_x_data.drop(beam_columns, axis=1)
+    
+    if 'transfer' not in training_method:
+        all_x_data['set'] = set_val
+    
+    for i in range(pred_index+1) :
+        
+        if i == 1 :
+            all_x_data['set'] = set_val
+        
+        model_name = 'db/%s_%s_%s' % (prediction_columns[i], ml_name, training_method)
+        model = pickle.load(open(model_name + ".pickle.dat", "rb"))
+        all_x_data[prediction_columns[i]] = model.predict(all_x_data)
+        
+        if i == 0 :
+            all_x_data["PCI"] = all_x_data["PCI"].apply(lambda x : pci_decode[x])
+        
 
 
-# In[30]:
+# In[6]:
 
 
-for prio, set_val in all_x_pci_dict :
-    all_x_pci = all_x_pci_dict[(prio, set_val)]
-    if not proba :
-        y_pred = model.predict(all_x_pci)
-        path = "../results/predicted/pci/%s/priority_%d_set_%d.png" % (ml_name, prio, set_val)
-        a = visualize_pci_heatmap(background, x_coord_view, y_coord_view, y_pred, path)
-    else :
-        y_pred=model.predict_proba(all_x_pci)
-        pci_interference = np.max(y_pred, axis=1)
-        normalize_pci_interference = matplotlib.colors.Normalize(vmin=min(pci_interference), 
-                                                                 vmax=max(pci_interference))
+all_y = all_x_data[prediction_columns[pred_index]]
 
-        pci_interference = [cmap(normalize_pci_interference(value))[:3] for value in pci_interference]
-        pci_interference = [[int(x*255) for x in value] for value in pci_interference]    
-        path = "../results/predicted/pci_interference/%s/confidence_pci/priority_%d_set_%d.png" %         (ml_name, prio, set_val)
-        a=visualize_all_location_heatmap(background, x_coord_view, y_coord_view, pci_interference, 
-                                         cmap, normalize_pci_interference, filename=path,
-                                         size=1, figsize=(20,10), adjustment=True, show=False)
+if normalized[pred_index] is None :
+    normalize = matplotlib.colors.Normalize(vmin=min(all_y), vmax=max(all_y))
+else :
+    normalize = normalized[pred_index]
+    
+data_pred = [cmap(normalize(value))[:3] for value in all_y]
+data_pred = [[int(x*255) for x in value] for value in data_pred]
+path = "../results/predicted/rsrp/%s/priority_%d_set_%d.png" % (ml_name, 6, set_val)
+a=visualize_all_location_heatmap(get_map_image(black_white=True), x_coord_view, y_coord_view, data_pred, 
+                                 cmap, normalize, filename=path,
+                                 size=1, figsize=(20,10), adjustment=True)
 
--- src/regressor-heatmap-generator.py
+++ src/regressor-heatmap-generator.py
@@ -23,7 +23,7 @@ warnings.filterwarnings('ignore')
 
 # # Predicted Data Preparation 
 
-# In[2]:
+# In[29]:
 
 
 x_cut = 50  
@@ -45,90 +45,72 @@ background = get_map_image(black_white=True)
 x_coord_view = [lon for lon in x_coord_list]
 y_coord_view = [lat for lat in y_coord_list]
 
-predicted_set_config = {6 : [1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-                             21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]}
-# predicted_set_config = {6 : [1]}
-
-
-# In[13]:
-
-
-prediction_columns = ["PCI", "RSRP", "RSRQ", "SNR"]
-pred_index = 1
-base_model = 'xgboost'
-pred_col = prediction_columns[pred_index]
-
-
-# In[6]:
-
-
-demo_config = {6 : [1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
-                    21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33]}
-# demo_config = {6 : [1]}
-
-df_all_data = get_data(config=demo_config, pure=True, refresh=False)
-print(len(df_all_data))
-
-
-# In[17]:
-
-
-beam_columns = [c for c in df_all_data if "beam" in c]
-dropped_columns = beam_columns + prediction_columns[pred_index+1:]
-df_data = df_all_data.drop(dropped_columns, axis=1)
-df_data = df_data[df_data["PCI"].isin(whitelist_PCI)]
-df_data = df_data.drop_duplicates()
-
-
-# In[11]:
-
-
-generated_function = [generate_predicted_data_pci, 
-                      generate_predicted_data_rsrp, 
+generated_function = [generate_predicted_data_rsrp, 
                       generate_predicted_data_rsrq, 
                       generate_predicted_data_snr]
 
 
-# In[14]:
-
-
-all_prev_y = load_from_pickle("predicted_%s_%s" % (prediction_columns[pred_index-1].lower(), base_model))
-all_x_data_dict = generated_function[pred_index](df_data, predicted_set_config, 
-                                                 x_coord_list, y_coord_list, all_prev_y, refresh=False)
-all_x_data_dict = {k:all_x_data_dict[k].drop(beam_columns, axis=1) for k in all_x_data_dict}
-all_x_data_dict = {k:all_x_data_dict[k].drop('set', axis=1) for k in all_x_data_dict}
-
-
 # # Predict 
 
-# In[34]:
+# In[27]:
 
 
+prediction_columns = ["RSRP", "RSRQ", "SNR"]
+pred_index = 0
 s = 2
+base_model = 'xgboost'
 ml_name = 'lgbm'
 training_method = 'baseline'  
 training_method = 'independent_set_%d' % (s) 
 training_method = 'transfer_except_%d' % (s) 
+
+
+# In[31]:
+
+
+config = {6 : [s]}
+pred_col = prediction_columns[pred_index]
 model_name = 'db/%s_%s_%s' % (pred_col, ml_name, training_method)
 model = pickle.load(open(model_name + ".pickle.dat", "rb"))
 
+df_all_data = get_data(config=config, pure=True, refresh=False)
+beam_columns = [c for c in df_all_data if "beam" in c]
+dropped_columns = beam_columns + prediction_columns[pred_index+1:]
+df_data = df_all_data.drop(dropped_columns, axis=1)
+df_data = df_data[df_data["PCI"].isin(whitelist_PCI)]
 
-# In[37]:
-
-
-all_y = model.predict(all_x_data_dict[(6, s)])
-
-
-# In[39]:
-
-
-p, s = 6, 20
-normalize = matplotlib.colors.Normalize(vmin=min(all_y), vmax=max(all_y))
-data_pred = [cmap(normalize(value))[:3] for value in all_y]
-data_pred = [[int(x*255) for x in value] for value in data_pred]
+for i in range(pred_index+1):
+    group = ['location_x', 'location_y', 'PCI', 'priority', 'set']
+    temp = df_data[group + [prediction_columns[i]]]
+    df_data[prediction_columns[i]] = merge_agg(temp, group, prediction_columns[i], ['mean'])['mean']
     
-path = "../results/predicted/rsrp/%s/priority_%d_set_%d.png" % (ml_name, 6, s)
-a=visualize_all_location_heatmap(get_map_image(black_white=True), x_coord_view, y_coord_view, data_pred, 
-                                 cmap, normalize, filename=path,
-                                 size=1, figsize=(20,10), adjustment=True)
+df_data = df_data.drop_duplicates()
+df_data = df_data.dropna()
+
+all_prev_y = load_from_pickle("predicted_%s_%s" % (prediction_columns[pred_index-1].lower(), base_model))
+all_x_data_dict = generated_function[pred_index](df_data, config, 
+                                                 x_coord_list, y_coord_list, 
+                                                 all_prev_y, refresh=False)
+all_x_data_dict = {k:all_x_data_dict[k].drop(beam_columns, axis=1) for k in all_x_data_dict}
+
+
+# In[33]:
+
+
+all_y_dict = {k:model.predict(all_x_data_dict[k]) for k in all_x_data_dict}
+
+
+# In[36]:
+
+
+for prio, set_val in all_y_dict :
+    all_y = all_y_dict[(prio, set_val)]
+    normalize = matplotlib.colors.Normalize(vmin=min(all_y), vmax=max(all_y))
+    data_pred = [cmap(normalize(value))[:3] for value in all_y]
+    data_pred = [[int(x*255) for x in value] for value in data_pred]
+
+    path = "../results/predicted/rsrp/%s/priority_%d_set_%d.png" % (ml_name, prio, set_val)
+    a=visualize_all_location_heatmap(get_map_image(black_white=True), x_coord_view, y_coord_view, data_pred, 
+                                     cmap, normalize, filename=path,
+                                     size=1, figsize=(20,10), adjustment=True)
 
